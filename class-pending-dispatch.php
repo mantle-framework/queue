5258d7b3852a6cb22b2d03819b2af3ba22176784
mantle-ci <mantle@alley.com>
2024-05-08 19:42:51 +0000

    Fix the method being called for Command_Event

--- class-pending-dispatch.php
+++ class-pending-dispatch.php
@@ -7,6 +7,7 @@
 
 namespace Mantle\Queue;
 
+use DateTimeInterface;
 use Mantle\Container\Container;
 use Mantle\Contracts\Queue\Dispatcher;
 use Mantle\Contracts\Queue\Job;
@@ -17,20 +18,11 @@ use RuntimeException;
  */
 class Pending_Dispatch {
 	/**
-	 * Job instance.
-	 *
-	 * @var Closure_Job|Job
-	 */
-	protected Closure_Job|Job $job; // phpcs:ignore Squiz.Commenting.VariableComment.Missing
-
-	/**
 	 * Constructor.
 	 *
 	 * @param Job|Closure_Job $job Job instance.
 	 */
-	public function __construct( $job ) {
-		$this->job = $job;
-	}
+	public function __construct( protected Job|Closure_Job $job ) {}
 
 	/**
 	 * Add a dispatch to a specific queue.
@@ -38,7 +30,6 @@ class Pending_Dispatch {
 	 * @throws RuntimeException If the job does not support queueing.
 	 *
 	 * @param string $queue Queue to add to.
-	 * @return static
 	 */
 	public function on_queue( string $queue ): Pending_Dispatch {
 		if ( ! method_exists( $this->job, 'on_queue' ) ) {
@@ -55,12 +46,11 @@ class Pending_Dispatch {
 	 *
 	 * @throws RuntimeException If the job does not support queueing.
 	 *
-	 * @param int $delay Delay in seconds.
-	 * @return static
+	 * @param DateTimeInterface|int $delay Delay in seconds or DateTime instance.
 	 */
-	public function delay( int $delay ): Pending_Dispatch {
+	public function delay( DateTimeInterface|int $delay ): Pending_Dispatch {
 		if ( ! method_exists( $this->job, 'delay' ) ) {
-			throw new RuntimeException( 'Job does not support queueing.' );
+			throw new RuntimeException( $this->job::class . ' does not support delayed queueing.' );
 		}
 
 		$this->job->delay( $delay );
